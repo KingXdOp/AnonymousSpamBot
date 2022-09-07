6cf5ea7c79e86e6b7bde1acd358745a45a65236c
N1xDOMINATOR <92099157+dominator454@users.noreply.github.com>
2022-09-07 11:41:25 +0000

    Add files via upload

---
+++ DollXSpamBot/plugins/start.py
@@ -0,0 +1,52 @@
+
+import asyncio
+import os
+from telethon import events, Button
+from telethon.tl.custom import button
+from DEADLYSPAM import BOT0, BOT1, BOT2, BOT3, BOT4, BOT5, BOT6, BOT7, BOT8, BOT9, ALIVE_PIC, OWNER_ID, OWNER_NAME
+
+DEADLY_IMG = ALIVE_PIC if ALIVE_PIC else "https://telegra.ph/file/f8d63b1dc5676fc9988f1.jpg"
+
+
+Deadly_Button = [
+        [
+        Button.url("Cʜᴀɴɴᴇʟ", "https://t.me/Dollx_spambot"),
+        Button.url("Sᴜᴘᴘᴏʀᴛ", "https://t.me/DollxSpam_BOT")
+        ],
+        [
+        Button.url("• Rᴇᴘᴏ •", "https://github.com/dominator454/DollXSpamBot")
+        ]
+        ]
+        
+
+#USERS 
+
+
+@BOT0.on(events.NewMessage(incoming=True, pattern='/start'))
+@BOT1.on(events.NewMessage(incoming=True, pattern='/start'))
+@BOT2.on(events.NewMessage(incoming=True, pattern='/start'))
+@BOT3.on(events.NewMessage(incoming=True, pattern='/start'))
+@BOT4.on(events.NewMessage(incoming=True, pattern='/start'))
+@BOT5.on(events.NewMessage(incoming=True, pattern='/start'))
+@BOT6.on(events.NewMessage(incoming=True, pattern='/start'))
+@BOT7.on(events.NewMessage(incoming=True, pattern='/start'))
+@BOT8.on(events.NewMessage(incoming=True, pattern='/start'))
+@BOT9.on(events.NewMessage(incoming=True, pattern='/start'))
+async def start(e):
+    if e.chat_id is e.sender_id:
+        name = e.sender.first_name
+        user_id = e.sender_id
+        mention = f"[{name}](tg://user?id={user_id})"
+        myOwner = f"[{OWNER_NAME}](tg://user?id={OWNER_ID})"
+        creator = f"[𝓓𝓞𝓛𝓛](tg://user?id={5143680336})"
+        DEADLY_ON = f"""
+ʜᴇʏ {mention},
+ᴛʜɪs ɪs ᴅᴏʟʟXsᴘᴀᴍʙᴏᴛ ᴘᴏᴡᴇʀᴇᴅ ʙʏ:- {creator}!
+
+ᴛʜɪs ʙᴏᴛ ᴏᴡɴᴇʀ:- {myOwner}
+
+ᴄᴏᴅᴇ ᴄʀᴇᴀᴛᴏʀ:- {creator}
+
+ᴄʟɪᴄᴋ ʙᴇʟᴏᴡ ʙᴜᴛᴛᴏɴ ᴛᴏ ᴀᴄᴄᴇss sᴜᴘᴘᴏʀᴛ ,ᴄʜᴀɴɴᴇʟ ᴀɴᴅ ʀᴇᴘᴏ!
+    """
+        await e.client.send_file(e.chat_id, DEADLY_IMG, caption=DEADLY_ON, buttons=Deadly_Button)
