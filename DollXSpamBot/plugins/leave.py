ba548eeb9935eee821f28067bd93d65fe0f46738
N1xDOMINATOR <92099157+dominator454@users.noreply.github.com>
2022-09-07 17:39:38 +0530

    Update leave.py

--- DollXSpamBot/plugins/leave.py
+++ DollXSpamBot/plugins/leave.py
@@ -1,6 +1,6 @@
 import asyncio
-from DEADLYSPAM import BOT0, BOT1, BOT2, BOT3, BOT4, BOT5, BOT6, BOT7, BOT8, BOT9, SUDO_USERS, OWNER_ID
-from DEADLYSPAM import CMD_HNDLR as hl
+from DollXSpamBot import BOT0, BOT1, BOT2, BOT3, BOT4, BOT5, BOT6, BOT7, BOT8, BOT9, SUDO_USERS, OWNER_ID
+from DollXSpamBot import CMD_HNDLR as hl
 import telethon.utils
 from telethon.tl import functions
 from telethon.tl.functions.channels import LeaveChannelRequest
