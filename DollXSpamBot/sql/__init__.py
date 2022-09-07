4a2ea0ed77ea90b18458b63b804e37bbbd1cb71a
N1xDOMINATOR <92099157+dominator454@users.noreply.github.com>
2022-09-07 17:48:33 +0530

    Update __init__.py

--- DollXSpamBot/sql/__init__.py
+++ DollXSpamBot/sql/__init__.py
@@ -6,7 +6,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 # Use Elephant Sql For Better Experience
-from DEADLYSPAM import DB_URI
+from DollXSpamBot import DB_URI
 
 
 def start() -> scoped_session:
